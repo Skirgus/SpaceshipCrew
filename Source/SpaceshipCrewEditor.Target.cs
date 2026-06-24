101f6b7b00dd53cdf0839f5a9f4e99551b616551
Smirnov Kirill <Skirgus@gmail.com>
2026-06-24 15:06:21 +0300

    chore(build): migrate project to Unreal Engine 5.8
    Update engine association and BuildSettings V7 required for UE 5.8 editor targets.

--- Source/SpaceshipCrewEditor.Target.cs
+++ Source/SpaceshipCrewEditor.Target.cs
@@ -6,7 +6,7 @@ public class SpaceshipCrewEditorTarget : TargetRules
 	public SpaceshipCrewEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("SpaceshipCrew");
 	}
